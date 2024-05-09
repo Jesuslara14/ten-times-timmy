83e98ff468556e26d82f07f8a55c2346626d4867
Jesuslara14 <111470200+Jesuslara14@users.noreply.github.com>
2024-05-09 14:27:06 -0700

    i hat ethis

--- src/game/main.js
+++ src/game/main.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('mainCanvas');
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 820, 570);
 
 function loadOffice(){
-    const image = new Image();
-    image.src = '../../public/backdrops/office.png'
+    let image = new Image();
+    image.src = './office.png';
+
+    ctx.drawImage(image, 50, 50);
 }
+
+loadOffice();
