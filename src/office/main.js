9e28f581b419e3d956cd6453b71f633959ca8263
IanHawkins <111469957+NovaHawkins@users.noreply.github.com>
2024-05-20 12:51:16 -0700

    Update main.js

--- src/office/main.js
+++ src/office/main.js
@@ -1,13 +1,28 @@
 const swipeButtons = document.getElementsByClassName('sideButton');
+const officeViewContainer = document.getElementById('officeViewContainer');
 
-const officeViews = document.getElementsByClassName('officeView');
-
-for(let i = 0; i < swipeButtons.length; i++){
+// Add event listeners to both buttons
+for (let i = 0; i < swipeButtons.length; i++) {
     swipeButtons[i].addEventListener('mouseover', (e) => swipe(e));
 }
 
-/* Office view functions */
+function swipe(event) {
+    const buttonId = event.target.id;
+    const containerWidth = officeViewContainer.offsetWidth;
+    let currentScrollPosition = officeViewContainer.scrollLeft;
 
-function swipe(event){
-    console.log(event.target.id);
+    // Determine swipe direction
+    if (buttonId === 'swipeLeft') {
+        // Move to the previous panel
+        currentScrollPosition -= containerWidth;
+    } else if (buttonId === 'swipeRight') {
+        // Move to the next panel
+        currentScrollPosition += containerWidth;
+    }
+
+    // Scroll to the new position smoothly
+    officeViewContainer.scrollTo({
+        left: currentScrollPosition,
+        behavior: 'smooth'
+    });
 }
